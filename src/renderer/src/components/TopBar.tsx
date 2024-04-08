a2e128632db9e7367a1d52f0fdac3e94a114370b
Howard Sun <komugi@gmail.com>
2024-04-08 00:12:47 -0400

    Changed to svg icons from tailwind
    Changes to be committed:
    	modified:   src/renderer/src/components/TopBar.tsx

--- src/renderer/src/components/TopBar.tsx
+++ src/renderer/src/components/TopBar.tsx
@@ -21,10 +21,36 @@ export default function TopBar(): JSX.Element {
           style={{ WebkitAppRegion: 'no-drag' } as React.CSSProperties}
         >
           <button id="minimize" onClick={handleMinimize}>
-            &#128469;
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
           </button>
           <button id="close" onClick={handleClose}>
-            &#10006;
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
           </button>
         </div>
       </div>
